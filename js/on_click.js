81738832f2e6fbd91ea9f448e544925e505e1f0e
fractalsin <zygmunt.pawel89@gmail.com>
2018-10-25 03:00:45 +0100

    html+css+js website

---
+++ js/on_click.js
@@ -0,0 +1,15 @@
+$("button").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".contact").offset().top},
+        'slow');
+});
+$(".o_nas").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".description").offset().top},
+        'slow');
+});
+$(".kontakt").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".contact").offset().top},
+        'slow');
+});
